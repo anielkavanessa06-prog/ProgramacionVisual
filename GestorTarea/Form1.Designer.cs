1bdb621a13de0c56b327c3ca8f82b94a8812d1c0
anielkavanessa06-prog <anielkavanessa06@gmail.com>
2025-09-04 17:13:33 -0600

    Add files via upload

---
+++ GestorTarea/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace GestorTarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblcodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.txtLugar = new System.Windows.Forms.TextBox();
+            this.lblLugar = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvTareas = new System.Windows.Forms.DataGridView();
+            this.txtBuscarCodigo = new System.Windows.Forms.TextBox();
+            this.cmbBuscarEstado = new System.Windows.Forms.ComboBox();
+            this.dtpFechaInicial = new System.Windows.Forms.DateTimePicker();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnMostrarTodo = new System.Windows.Forms.Button();
+            this.btnGuardarCambios = new System.Windows.Forms.Button();
+            this.dtpFechaFin = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTareas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblcodigo
+            // 
+            this.lblcodigo.AutoSize = true;
+            this.lblcodigo.Location = new System.Drawing.Point(19, 58);
+            this.lblcodigo.Name = "lblcodigo";
+            this.lblcodigo.Size = new System.Drawing.Size(63, 20);
+            this.lblcodigo.TabIndex = 0;
+            this.lblcodigo.Text = "Codigo:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(108, 58);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(112, 26);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(108, 15);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(112, 26);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(14, 15);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(69, 20);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(-12, 96);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(96, 20);
+            this.lblDescripcion.TabIndex = 4;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(108, 96);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(112, 74);
+            this.txtDescripcion.TabIndex = 5;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(-1, 179);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(166, 20);
+            this.lblFecha.TabIndex = 6;
+            this.lblFecha.Text = "Fecha de Realización:";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(164, 179);
+            this.dtpFecha.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(224, 26);
+            this.dtpFecha.TabIndex = 7;
+            // 
+            // txtLugar
+            // 
+            this.txtLugar.Location = new System.Drawing.Point(108, 215);
+            this.txtLugar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtLugar.Name = "txtLugar";
+            this.txtLugar.Size = new System.Drawing.Size(112, 26);
+            this.txtLugar.TabIndex = 9;
+            // 
+            // lblLugar
+            // 
+            this.lblLugar.AutoSize = true;
+            this.lblLugar.Location = new System.Drawing.Point(30, 215);
+            this.lblLugar.Name = "lblLugar";
+            this.lblLugar.Size = new System.Drawing.Size(54, 20);
+            this.lblLugar.TabIndex = 8;
+            this.lblLugar.Text = "Lugar:";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(20, 250);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(64, 20);
+            this.lblEstado.TabIndex = 10;
+            this.lblEstado.Text = "Estado:";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Items.AddRange(new object[] {
+            "No realizada",
+            "En proceso",
+            "Terminda"});
+            this.cmbEstado.Location = new System.Drawing.Point(108, 250);
+            this.cmbEstado.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(136, 28);
+            this.cmbEstado.TabIndex = 11;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(2, 356);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(124, 86);
+            this.btnAgregar.TabIndex = 12;
+            this.btnAgregar.Text = "Agregar Tarea";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(133, 356);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(119, 86);
+            this.btnEditar.TabIndex = 13;
+            this.btnEditar.Text = "Editar Tarea";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(259, 356);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(116, 86);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "Eliminar Tarea";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvTareas
+            // 
+            this.dgvTareas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvTareas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTareas.Location = new System.Drawing.Point(396, 15);
+            this.dgvTareas.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvTareas.MultiSelect = false;
+            this.dgvTareas.Name = "dgvTareas";
+            this.dgvTareas.RowHeadersWidth = 51;
+            this.dgvTareas.RowTemplate.Height = 24;
+            this.dgvTareas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvTareas.Size = new System.Drawing.Size(810, 409);
+            this.dgvTareas.TabIndex = 15;
+            this.dgvTareas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTareas_CellContentClick);
+            // 
+            // txtBuscarCodigo
+            // 
+            this.txtBuscarCodigo.Location = new System.Drawing.Point(451, 471);
+            this.txtBuscarCodigo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtBuscarCodigo.Name = "txtBuscarCodigo";
+            this.txtBuscarCodigo.Size = new System.Drawing.Size(112, 26);
+            this.txtBuscarCodigo.TabIndex = 16;
+            // 
+            // cmbBuscarEstado
+            // 
+            this.cmbBuscarEstado.FormattingEnabled = true;
+            this.cmbBuscarEstado.Items.AddRange(new object[] {
+            "No realizada",
+            "En proceso",
+            "Terminda"});
+            this.cmbBuscarEstado.Location = new System.Drawing.Point(427, 524);
+            this.cmbBuscarEstado.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbBuscarEstado.Name = "cmbBuscarEstado";
+            this.cmbBuscarEstado.Size = new System.Drawing.Size(136, 28);
+            this.cmbBuscarEstado.TabIndex = 17;
+            // 
+            // dtpFechaInicial
+            // 
+            this.dtpFechaInicial.Location = new System.Drawing.Point(586, 432);
+            this.dtpFechaInicial.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpFechaInicial.Name = "dtpFechaInicial";
+            this.dtpFechaInicial.Size = new System.Drawing.Size(224, 26);
+            this.dtpFechaInicial.TabIndex = 18;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(672, 504);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(138, 66);
+            this.btnBuscar.TabIndex = 20;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnMostrarTodo
+            // 
+            this.btnMostrarTodo.Location = new System.Drawing.Point(843, 504);
+            this.btnMostrarTodo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnMostrarTodo.Name = "btnMostrarTodo";
+            this.btnMostrarTodo.Size = new System.Drawing.Size(177, 66);
+            this.btnMostrarTodo.TabIndex = 21;
+            this.btnMostrarTodo.Text = "Mostrar Todo";
+            this.btnMostrarTodo.UseVisualStyleBackColor = true;
+            this.btnMostrarTodo.Click += new System.EventHandler(this.btnMostrarTodo_Click);
+            // 
+            // btnGuardarCambios
+            // 
+            this.btnGuardarCambios.Location = new System.Drawing.Point(133, 450);
+            this.btnGuardarCambios.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGuardarCambios.Name = "btnGuardarCambios";
+            this.btnGuardarCambios.Size = new System.Drawing.Size(129, 79);
+            this.btnGuardarCambios.TabIndex = 22;
+            this.btnGuardarCambios.Text = "Guardar Cambios";
+            this.btnGuardarCambios.UseVisualStyleBackColor = true;
+            this.btnGuardarCambios.Click += new System.EventHandler(this.btnGuardarCambios_Click);
+            // 
+            // dtpFechaFin
+            // 
+            this.dtpFechaFin.Location = new System.Drawing.Point(816, 432);
+            this.dtpFechaFin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpFechaFin.Name = "dtpFechaFin";
+            this.dtpFechaFin.Size = new System.Drawing.Size(224, 26);
+            this.dtpFechaFin.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 585);
+            this.Controls.Add(this.dtpFechaFin);
+            this.Controls.Add(this.btnGuardarCambios);
+            this.Controls.Add(this.btnMostrarTodo);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dtpFechaInicial);
+            this.Controls.Add(this.cmbBuscarEstado);
+            this.Controls.Add(this.txtBuscarCodigo);
+            this.Controls.Add(this.dgvTareas);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.cmbEstado);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.txtLugar);
+            this.Controls.Add(this.lblLugar);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblcodigo);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTareas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblcodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.TextBox txtLugar;
+        private System.Windows.Forms.Label lblLugar;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgvTareas;
+        private System.Windows.Forms.TextBox txtBuscarCodigo;
+        private System.Windows.Forms.ComboBox cmbBuscarEstado;
+        private System.Windows.Forms.DateTimePicker dtpFechaInicial;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnMostrarTodo;
+        private System.Windows.Forms.Button btnGuardarCambios;
+        private System.Windows.Forms.DateTimePicker dtpFechaFin;
+    }
+}
+
---
+++ GestorTarea/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GestorTarea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Tarea
+        {
+            public string Codigo { get; set; }
+            public string Nombre { get; set; }
+            public string Descripcion { get; set; }
+            public DateTime Fecha { get; set; }
+            public string Lugar { get; set; }
+            public string Estado { get; set; }
+        }
+
+        List<Tarea> listaTareas = new List<Tarea>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbEstado.Items.Clear();
+            cmbEstado.Items.AddRange(new string[] { "Pendiente", "En Proceso", "Completada" });
+
+            cmbBuscarEstado.Items.Clear();
+            cmbBuscarEstado.Items.AddRange(new string[] { "Pendiente", "En Proceso", "Completada" });
+
+            dtpFechaInicial.Format = DateTimePickerFormat.Short;
+            dtpFechaFin.Format = DateTimePickerFormat.Short;
+        }
+
+        private void ActualizarGrid()
+        {
+            dgvTareas.DataSource = null;
+            dgvTareas.DataSource = listaTareas;
+        }
+
+        private void LimpiarCampos()
+        {
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+            txtLugar.Clear();
+            cmbEstado.SelectedIndex = -1;
+            txtBuscarCodigo.Clear();
+            cmbBuscarEstado.SelectedIndex = -1;
+            dtpFecha.Value = DateTime.Now;
+            dtpFechaInicial.Value = DateTime.Now;
+            dtpFechaFin.Value = DateTime.Now;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCodigo.Text) ||
+                string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                cmbEstado.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, complete los campos obligatorios.");
+                return;
+            }
+
+            Tarea nueva = new Tarea()
+            {
+                Codigo = txtCodigo.Text,
+                Nombre = txtNombre.Text,
+                Descripcion = txtDescripcion.Text,
+                Fecha = dtpFecha.Value,
+                Lugar = txtLugar.Text,
+                Estado = cmbEstado.SelectedItem.ToString()
+            };
+
+            listaTareas.Add(nueva);
+            ActualizarGrid();
+            MessageBox.Show("Tarea agregada correctamente.");
+            LimpiarCampos();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dgvTareas.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Por favor, selecciona una tarea en la tabla para editar.");
+                return;
+            }
+
+            int index = dgvTareas.SelectedRows[0].Index;
+
+            if (index >= 0 && index < listaTareas.Count)
+            {
+                Tarea tareaSeleccionada = listaTareas[index];
+
+                txtCodigo.Text = tareaSeleccionada.Codigo;
+                txtNombre.Text = tareaSeleccionada.Nombre;
+                txtDescripcion.Text = tareaSeleccionada.Descripcion;
+                dtpFecha.Value = tareaSeleccionada.Fecha;
+                txtLugar.Text = tareaSeleccionada.Lugar;
+
+                if (cmbEstado.Items.Contains(tareaSeleccionada.Estado))
+                {
+                    cmbEstado.SelectedItem = tareaSeleccionada.Estado;
+                }
+                else
+                {
+                    cmbEstado.SelectedIndex = -1;
+                }
+
+                MessageBox.Show("Datos cargados. Ahora podés modificar y guardar los cambios.");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvTareas.SelectedRows.Count > 0)
+            {
+                int index = dgvTareas.SelectedRows[0].Index;
+                listaTareas.RemoveAt(index);
+                ActualizarGrid();
+                MessageBox.Show("Tarea eliminada correctamente.");
+                LimpiarCampos();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            BuscarTareas();
+        }
+
+        private void BuscarTareas()
+        {
+            List<Tarea> resultado = listaTareas;
+
+            // Buscar solo por código si se ingresó
+            if (!string.IsNullOrWhiteSpace(txtBuscarCodigo.Text))
+            {
+                resultado = resultado
+                    .Where(t => t.Codigo.Equals(txtBuscarCodigo.Text, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
+            // Buscar solo por estado si se seleccionó
+            else if (cmbBuscarEstado.SelectedItem != null)
+            {
+                resultado = resultado
+                    .Where(t => t.Estado.Equals(cmbBuscarEstado.SelectedItem.ToString(), StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
+            // Buscar solo por rango de fechas si se seleccionó un rango válido
+            else
+            {
+                DateTime inicio = dtpFechaInicial.Value.Date;
+                DateTime fin = dtpFechaFin.Value.Date;
+
+                if (inicio > fin)
+                {
+                    MessageBox.Show("La fecha de inicio no puede ser mayor que la fecha final.", "Rango inválido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                resultado = resultado
+                    .Where(t => t.Fecha.Date >= inicio && t.Fecha.Date <= fin)
+                    .ToList();
+            }
+
+            dgvTareas.DataSource = null;
+            dgvTareas.DataSource = resultado;
+        }
+
+        private void btnMostrarTodo_Click(object sender, EventArgs e)
+        {
+            ActualizarGrid();
+            LimpiarCampos();
+        }
+
+        private void dgvTareas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                txtCodigo.Text = dgvTareas.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtNombre.Text = dgvTareas.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtDescripcion.Text = dgvTareas.Rows[e.RowIndex].Cells[2].Value.ToString();
+                dtpFecha.Value = (DateTime)dgvTareas.Rows[e.RowIndex].Cells[3].Value;
+                txtLugar.Text = dgvTareas.Rows[e.RowIndex].Cells[4].Value.ToString();
+
+                string estado = dgvTareas.Rows[e.RowIndex].Cells[5].Value.ToString();
+                if (cmbEstado.Items.Contains(estado))
+                {
+                    cmbEstado.SelectedItem = estado;
+                }
+                else
+                {
+                    cmbEstado.SelectedIndex = -1;
+                }
+            }
+        }
+
+        private void btnGuardarCambios_Click(object sender, EventArgs e)
+        {
+            if (dgvTareas.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecciona una tarea para guardar los cambios.");
+                return;
+            }
+
+            int index = dgvTareas.SelectedRows[0].Index;
+
+            if (index >= 0 && index < listaTareas.Count)
+            {
+                listaTareas[index].Codigo = txtCodigo.Text;
+                listaTareas[index].Nombre = txtNombre.Text;
+                listaTareas[index].Descripcion = txtDescripcion.Text;
+                listaTareas[index].Fecha = dtpFecha.Value;
+                listaTareas[index].Lugar = txtLugar.Text;
+                listaTareas[index].Estado = cmbEstado.SelectedItem?.ToString() ?? "";
+
+                ActualizarGrid();
+                MessageBox.Show("Cambios guardados correctamente.");
+                LimpiarCampos();
+            }
+        }
+    }
+}
